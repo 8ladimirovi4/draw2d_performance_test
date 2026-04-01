33d9109f5bdd677cfd7cfd6dfffe0c7e5d9816bc
sea4836-systeme-ru <vladimir.leonov@systeme.ru>
2026-04-01 19:21:34 +0300

    feat: create datatable for big fixture

--- client/js/schema-loader.js
+++ client/js/schema-loader.js
@@ -22,8 +22,36 @@
     color: true,
     stroke: true,
     radius: true,
-    dasharray: true
+    dasharray: true,
+    /** Fixture-only: filled via addRow (see applyTableBoxRowsFromElement). */
+    rows: true
   };
+
+  function applyTableBoxRowsFromElement(canvas, element) {
+    if (!element || element.type !== 'draw2d.shape.box.TableBox') {
+      return;
+    }
+    if (!Array.isArray(element.rows) || element.rows.length === 0) {
+      return;
+    }
+    var fig = canvas.getFigure(element.id);
+    if (!fig || typeof fig.addRow !== 'function' || typeof fig.removeRow !== 'function') {
+      return;
+    }
+    while (fig.getRowCount() > 0) {
+      fig.removeRow(0);
+    }
+    var r;
+    for (r = 0; r < element.rows.length; r++) {
+      var row = element.rows[r];
+      if (Array.isArray(row) && row.length > 0) {
+        fig.addRow.apply(fig, row);
+      }
+    }
+    if (typeof fig.forceLayout === 'function') {
+      fig.forceLayout();
+    }
+  }
 
   function applyLabelLikeDeclaredWidth(fig, element) {
     var name = fig.NAME || '';
@@ -55,8 +83,12 @@
     }
     var patch = {};
     var key;
+    var figName = fig.NAME || '';
     for (key in element) {
       if (!Object.prototype.hasOwnProperty.call(element, key) || MEMENTO_KEYS[key]) {
+        continue;
+      }
+      if (figName === 'draw2d.shape.box.TableBox' && key === 'grid' && typeof element.grid === 'boolean') {
         continue;
       }
       if (fig.setterWhitelist[key]) {
@@ -90,6 +122,7 @@
     var i;
     for (i = 0; i < data.length; i++) {
       applyWhitelistAttrsFromElement(this.canvas, data[i]);
+      applyTableBoxRowsFromElement(this.canvas, data[i]);
     }
   };
 
